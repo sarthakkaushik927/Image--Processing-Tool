052a8af1402211e4fd92be4b89e292bfae86b6b4
sarthakkaushik927 <sarthakkaushik927@gmail.com>
2025-11-03 13:54:53 +0530

    more errors

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import YOLOUploader from './pages/YOLOUploader.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/image-detect" element={<YOLOUploader />} />
+        
       </Routes>
     </BrowserRouter>
   </StrictMode>,
